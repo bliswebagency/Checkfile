c74deb84642bbf0a62479f654cefb1c4712066ab
Sam Lomax <sam@blis.net.au>
2011-08-26 22:50:46 +0930

    Initial Commit

---
+++ checkfile/pi.checkfile.php
@@ -0,0 +1,74 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		ExpressionEngine Dev Team
+ * @copyright	Copyright (c) 2003 - 2011, EllisLab, Inc.
+ * @license		http://expressionengine.com/user_guide/license.html
+ * @link		http://expressionengine.com
+ * @since		Version 2.0
+ * @filesource
+ */
+ 
+// ------------------------------------------------------------------------
+
+/**
+ * Checkfile Plugin
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Addons
+ * @category	Plugin
+ * @author		Blis Web Agency
+ * @link		http://blis.net.au
+ */
+
+$plugin_info = array(
+	'pi_name'		=> 'Checkfile',
+	'pi_version'	=> '1.0',
+	'pi_author'		=> 'Blis Web Agency',
+	'pi_author_url'	=> 'http://blis.net.au',
+	'pi_description'=> 'Checks if a file exists, then falls back on a remote, then gives up!',
+	'pi_usage'		=> Checkfile::usage()
+);
+
+
+class Checkfile {
+
+	public $return_data;
+    
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		$this->EE =& get_instance();
+	}
+	
+	// ----------------------------------------------------------------
+	
+	/**
+	 * Plugin Usage
+	 */
+	public static function usage()
+	{
+		ob_start();
+?>
+
+This plugin is designed for people who have a files which are in one place which really need to be in another. Here's how it works.
+
+First, it checks if the files are where you want them on Server A.
+If not, it checks Server B. If it finds them there, it copies them to where they need to be on Server A.
+
+Failing this, will then either serve a default file (e.g. "no-image.jpg") or nothing at all.
+<?php
+		$buffer = ob_get_contents();
+		ob_end_clean();
+		return $buffer;
+	}
+}
+
+
+/* End of file pi.checkfile.php */
+/* Location: /system/expressionengine/third_party/checkfile/pi.checkfile.php */
